6ae2e02649200700f87304de3e6b2c25c0be1555
msabbar02 <msabbar1@educacion.navarra.es>
2025-04-14 00:52:56 +0200

    mejorado las pruebas en CuentaTest.java con casos de prueba para los métodos getNombre, getSaldo, depositar y retirar

--- src/test/java/org/example/CuentaTest.java
+++ src/test/java/org/example/CuentaTest.java
@@ -6,25 +6,40 @@ class CuentaTest {
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        Cuenta cuenta = new Cuenta("Juan", 1000.0);
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        Cuenta cuenta = null;
     }
 
     @org.junit.jupiter.api.Test
     void getNombre() {
+        Cuenta cuenta = new Cuenta("Juan", 1000.0);
+        assertEquals("Juan", cuenta.getNombre());
+        assertNotEquals("Pedro", cuenta.getNombre());
     }
 
     @org.junit.jupiter.api.Test
     void getSaldo() {
+        Cuenta cuenta = new Cuenta("Juan", 1000.0);
+        assertTrue(cuenta.getSaldo() > 1000.0);
+        assertFalse(cuenta.getSaldo() < 1000.0);
     }
 
     @org.junit.jupiter.api.Test
     void depositar() {
+        Cuenta cuenta = new Cuenta("Juan", 1000.0);
+        cuenta.depositar(500.0);
+        assertEquals(1500.0, cuenta.getSaldo());
     }
 
     @org.junit.jupiter.api.Test
     void retirar() {
+        Cuenta cuenta = null;
+        assertNull(cuenta,"La cuenta no debe ser nula");
+        cuenta.retirar(200.0);
+
     }
 }
