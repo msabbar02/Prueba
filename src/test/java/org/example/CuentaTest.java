371cf8b192d56c6cc6141d9852e72e4fb860bb39
msabbar02 <msabbar1@educacion.navarra.es>
2025-04-14 00:25:47 +0200

    he generado el clase CuentaTest.java para hacer pruebas y por el pom.xml he anadido la dependencia de Junit .

---
+++ src/test/java/org/example/CuentaTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNombre() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSaldo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void depositar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void retirar() {
+    }
+}
