722bc53366dc201ecc25b05607c7117c3aab9d93
msabbar02 <msabbar1@educacion.navarra.es>
2025-04-14 00:20:25 +0200

    he creado la clase cuenta con sus atributos

--- src/main/java/org/example/Cuenta.java
+++ src/main/java/org/example/Cuenta.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Cuenta {
+    private String nombre;
+    private double saldo;
+
+    public Cuenta(String nombre, double saldo) {
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+        }
+    }
 }
